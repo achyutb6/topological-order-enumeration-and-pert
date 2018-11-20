070723636de57f8caea44a44757b513028a64042
Achyut <achyut.b6@gmail.com>
2018-11-19 21:25:51 -0600

    Initial commit

---
+++ src/aab180004/DFS.java
@@ -0,0 +1,199 @@
+/** Starter code for SP8
+ *  @author 
+ */
+
+package aab180004;
+
+import rbk.Graph.Vertex;
+import rbk.Graph;
+import rbk.Graph.Edge;
+import rbk.Graph.GraphAlgorithm;
+import rbk.Graph.Factory;
+import rbk.Graph.Timer;
+
+import java.io.File;
+import java.util.List;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+/**
+ * The class represents the implementation of Depth first Search 
+ * @author Mythri Thippareddy, Achyut Bhandiwad
+ *
+ */
+/**
+ * @author Mythri Thippareddy
+ *
+ */
+public class DFS extends GraphAlgorithm<DFS.DFSVertex> {
+
+	private LinkedList<Vertex> finishList; // Output of dfs() will be stored in finishList
+	public static boolean isCyclic; // isCyclic check
+	public int numberOfConnectedComponents; // The number of connected components of the graph as 0;
+	private int topNum; // 	Used for dfs()
+
+	/**
+	 * @author Mythri Thippareddy, Achyut Bhandiwad
+	 *
+	 */
+	public static class DFSVertex implements Factory {
+		int cno; // component number
+		public boolean seen; // check if the vertex is seen
+		public Vertex parent; // storing the parent of the vertex
+		int top; // storing the top number to check for back edges
+
+		/**
+		 * @param u
+		 * converting vertex u to DFSVertex
+		 */
+		public DFSVertex(Vertex u) {
+			this.seen = false; 
+			this.parent = null;
+			this.top = 0;
+			this.cno = 0;
+		}
+
+		/* 
+		 * @see rbk.Graph.Factory#make(rbk.Graph.Vertex)
+		 */
+		public DFSVertex make(Vertex u) {
+			return new DFSVertex(u);
+		}
+	}
+
+	/**
+	 * @param g 
+	 * Initialing g with numberOfConnectedComponents and isCyclic
+	 */
+	public DFS(Graph g) {
+		super(g, new DFSVertex(null));
+		isCyclic = false;
+		numberOfConnectedComponents=0;
+	}
+
+	/**
+	 * @param g
+	 * @return DFS object with the objects ordered
+	 */
+	public static DFS depthFirstSearch(Graph g) {
+		DFS d = new DFS(g);
+		d.dfs();
+		return d;
+	}
+
+	/**
+	 * Dfs method to run depth first search
+	 */
+	public void dfs() {
+		topNum = g.size();
+		for (Vertex u : g) {
+			get(u).seen = false;
+			get(u).parent = null;
+			get(u).top = 0;
+		}
+		finishList = new LinkedList<Vertex>();
+		for (Vertex u : g) {
+			if (!get(u).seen) {
+				get(u).cno = ++numberOfConnectedComponents;
+				DFS_visit(u);
+			}
+		}
+	}
+
+	/**
+	 * @param u
+	 * Visiting each node adjacent to Vertex u
+	 */
+	private void DFS_visit(Vertex u) {
+		get(u).seen = true;
+		for (Edge e : g.incident(u)) {
+			Vertex v = e.otherEnd(u);
+			if (!get(v).seen) {
+				get(v).parent = u;
+				get(v).cno = get(u).cno;
+				DFS_visit(v);
+			} else {
+				if (get(v).top == 0) {
+					isCyclic = true;
+				}
+			}
+		}
+		get(u).top = topNum--;
+		finishList.addFirst(u);
+	}
+
+	
+	/**
+	 * @return list of vertices in topological order
+	 */
+	public List<Vertex> topologicalOrder1() {
+		dfs();
+		if(isCyclic) {
+			return null;
+		}
+		return finishList;
+	}
+	
+	/**
+	 * @return the number of connected components
+	 */
+	public int connectedComponents() {
+		dfs();
+		return numberOfConnectedComponents;
+	}
+
+	/**
+	 * @param u
+	 * @return the component number of the vertex u
+	 */
+	public int cno(Vertex u) {
+		return get(u).cno;
+	}
+
+	/**
+	 * @param g
+	 * @return Returns null if g is not a DAG else return the topological oder of a DAG using DFS. 
+	 */
+	public static List<Vertex> topologicalOrder1(Graph g) {
+		DFS d = new DFS(g);
+		return d.topologicalOrder1();
+	}
+
+	public static void main(String[] args) throws Exception {
+		String string = "7 8   1 2 2   1 3 3   2 4 5   3 4 4   4 5 1   5 1 7   6 7 1   7 6 1 0";
+		Scanner in;
+		// If there is a command line argument, use it as file from which
+		// input is read, otherwise use input from string.
+		in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(string);
+
+		// Read graph from input
+		Graph g = Graph.readGraph(in);
+		g.printGraph(false);
+
+		DFS d = new DFS(g);
+		int numcc = d.connectedComponents();
+		System.out.println("Number of components: " + numcc + "\nu\tcno");
+		for (Vertex u : g) {
+			System.out.println(u + "\t" + d.cno(u));
+		}
+		System.out.println("");
+		
+		System.out.println("Output of Dfs:\nNode\ttop\tParent\n----------------------");
+		for (Vertex u : g) {
+			System.out.println(u + "\t" + d.get(u).top + "\t" + d.get(u).parent);
+		}
+		System.out.println("");
+		System.out.println("Output of topological order\n-----------------------------");
+		List<Vertex> toplogicalList = d.topologicalOrder1();
+		if(toplogicalList==null) {
+			System.out.println("Graph is cyclic. No topological order exists");
+		}else {
+			for(Vertex u: toplogicalList) {
+				System.out.print(u+"\t");
+			}
+			System.out.println("");
+		}
+	}
+}
---
+++ src/aab180004/PERT.java
@@ -0,0 +1,149 @@
+/* Driver code for PERT algorithm (LP4)
+ * @author
+ */
+
+// change package to your netid
+package aab180004;
+
+import rbk.Graph;
+import rbk.Graph.Vertex;
+import rbk.Graph.Edge;
+import rbk.Graph.GraphAlgorithm;
+import rbk.Graph.Factory;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
+
+public class PERT extends GraphAlgorithm<PERT.PERTVertex> {
+	int maxTime;
+	int criticalPathLength;
+	int numOfCriticalNodes;
+	Vertex s;
+	Vertex t;
+
+    public static class PERTVertex implements Factory {
+    	int duration;
+    	int ec;
+    	int lc;
+    	int slack;
+    	boolean isCritical;
+	public PERTVertex(Vertex u) {
+		this.ec = 0;
+		this.isCritical = false;
+		this.slack = 0;
+	}
+	public PERTVertex make(Vertex u) { return new PERTVertex(u); }
+    }
+
+    public PERT(Graph g) {
+		super(g, new PERTVertex(null));
+		this.maxTime =0;
+		this.criticalPathLength = 0;
+		this.numOfCriticalNodes = 0;
+		this.s = g.getVertex(1);
+		this.t = g.getVertex(g.size());
+    }
+
+    public void setDuration(Vertex u, int d) {
+		get(u).duration = d;
+    }
+    
+    public boolean pert() {
+		DFS d = new DFS(g);
+		List<Vertex> topologicalOrder = d.topologicalOrder1();
+		if(topologicalOrder == null){
+			return false;
+		}
+		for( Vertex u : topologicalOrder){
+		    for(Edge e : g.incident(u)){
+                Vertex v = e.otherEnd(u);
+                if(get(u).ec + get(v).duration > get(v).ec){
+                    get(v).ec = get(u).ec + get(v).duration;
+                }
+            }
+        }
+        maxTime = get(t).ec;
+		for(Vertex u : g){
+		    get(u).lc = maxTime;
+        }
+
+        ListIterator<Vertex> reverseTopological = topologicalOrder.listIterator(topologicalOrder.size());
+        while(reverseTopological.hasPrevious()){
+            Vertex u = reverseTopological.previous();
+            for( Edge e : g.incident(u)){
+                Vertex v = e.otherEnd(u);
+                if(get(v).lc - get(v).duration < get(u).lc){
+                    get(u).lc = get(v).lc - get(v).duration;
+                }
+            }
+            get(u).slack = get(u).lc - get(u).ec;
+        }
+		return true;
+    }
+    public int ec(Vertex u) {
+	return 0;
+    }
+
+    public int lc(Vertex u) {
+	return 0;
+    }
+
+    public int slack(Vertex u) {
+	return 0;
+    }
+
+    public int criticalPath() {
+	return 0;
+    }
+
+    public boolean critical(Vertex u) {
+	return false;
+    }
+
+    public int numCritical() {
+	return 0;
+    }
+
+    // setDuration(u, duration[u.getIndex()]);
+    public static PERT pert(Graph g, int[] duration) {
+		PERT pert = new PERT(g);
+        for(Vertex u: g) {
+            pert.setDuration(u, duration[u.getIndex()]);
+            g.addEdge(pert.s.getIndex(),u.getIndex(),0);
+            g.addEdge(u.getIndex(),pert.t.getIndex(),0);
+        }
+        if(pert.pert()){
+            return pert;
+        }else{
+            return null;
+        }
+    }
+    
+    public static void main(String[] args) throws Exception {
+	String graph = "11 12   2 4 1   2 5 1   3 5 1   3 6 1   4 7 1   5 7 1   5 8 1   6 8 1   6 9 1   7 10 1   8 10 1   9 10 1      0 3 2 3 2 1 3 2 4 1 0";
+	Scanner in;
+	// If there is a command line argument, use it as file from which
+	// input is read, otherwise use input from string.
+	in = args.length > 0 ? new Scanner(new File(args[0])) : new Scanner(graph);
+	Graph g = Graph.readDirectedGraph(in);
+	g.printGraph(false);
+	
+	PERT p = new PERT(g);
+	for(Vertex u: g) {
+	    p.setDuration(u, in.nextInt());
+	}
+	// Run PERT algorithm.  Returns null if g is not a DAG
+	if(p.pert()) {
+	    System.out.println("Invalid graph: not a DAG");
+	} else {
+	    System.out.println("Number of critical vertices: " + p.numCritical());
+	    System.out.println("u\tEC\tLC\tSlack\tCritical");
+	    for(Vertex u: g) {
+		System.out.println(u + "\t" + p.ec(u) + "\t" + p.lc(u) + "\t" + p.slack(u) + "\t" + p.critical(u));
+	    }
+	}
+    }
+}
